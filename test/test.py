ec197caea46475c3cc8bf4edefdf72de71310ce0
kaiserchenlk <1097502565@qq.com>
2019-11-14 16:43:47 +0800

    更新string返回函数

--- test/test.py
+++ test/test.py
@@ -1,39 +1,36 @@
 #返回陈述知识字符串
-#knowname知识名，knowclass知识类，knowslot知识槽
-#列表元素为字符串型
-def getprint2(knowname,knowclass,knowslot):
+#knowstatement为陈述知识
+#传单个列表，列表元素为三个元素的元组
+def getprint2(knowstatement):
     prints2 = ''
     prints2 += '(chunk-type count-order first second)\n'
     prints2 += '(chunk-type count-from first second)\n'
     prints2 += '(add-dm\n'
     i=0
-    while i<len(knowname):
-        prints2 += ' ('+str(knowname[i])+' ISA '+str(knowclass[i])+' '+str(knowslot[i])+')\n'
+    while i<len(knowstatement):
+        prints2 += ' ('+str(knowstatement[i][0])+' ISA '+str(knowstatement[i][1])+' '+str(knowstatement[i][2])+')\n'
         i+=1
     prints2 += ')'
     return prints2
 
 #test
-kon=['b','c','d','e','f','first-goal']
-knc=['count-order','count-order','count-order','count-order','count-order','count-from']
-kns=['first 1 second 2','first 2 second 3','first 3 second 4','first 4 second 5','first 5 second 6','start 2 end 4']
-print(getprint2(kon,knc,kns))
+know=[('b','count-order','first 1 second 2'),('c','count-order','first 2 second 3'),('d','count-order','first 3 second 4')]
+print(getprint2(know))
 
 
 #返回系统参数字符串
-#pa参数名，pavalue参数值
-#列表元素为字符串型
-def getprint1(pa,pavalue):
+#sysparameter为系统参数
+#传一个列表，列表元素为两个元素的元组
+def getprint1(sysparameter):
         prints =''
         prints +='(sgp'
         i=0
-        while i<len(pa):
-            prints +=' '+':'+str(pa[i])+" "+str(pavalue[i])
+        while i<len(sysparameter):
+            prints +=' '+':'+str(sysparameter[i][0])+" "+str(sysparameter[i][1])
             i+=1
         prints +=')'
         return prints
 
 #test
-a=['esc','If','trace-detail']
-b=['t','.05','high']
-print(getprint1(a,b))
+a=[('esc',.05 ), ('if','high') ]
+print(getprint1(a))
