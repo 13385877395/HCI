c15621ac58e9f99db6d9225d2d161d1d853a4f92
aierlan6 <1225008708@qq.com>
2020-05-11 16:05:19 +0800

    无

--- 动作/行为分析g.py
+++ 动作/行为分析g.py
@@ -18,30 +18,29 @@ class actForm(QtWidgets.QWidget, Ui_Frame):
     def __init__(self):
         super(actForm, self).__init__()
         self.setupUi(self)
-        self.names = []
         # 创建一个对象，存储多个模型函数
-        self.models = proModeling()
+        self.models = {}
+        self.tempModel = {}
 
         # 手工建模
-        self.pushButton_4.clicked.connect(self.Preview1)
-        self.pushButton_3.clicked.connect(self.add1)
-        self.listWidget_4.itemClicked.connect(self.QLWclicked1)
+        self.pushButton_4.clicked.connect(lambda: self.Preview(self.mmcondition, self.mmresult, self.lineEdit, self.textBrowser_4))
+        self.pushButton_3.clicked.connect(self.add)
+        self.listWidget_4.itemClicked.connect(self.QLWclicked)
         self.listWidget_4.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
         self.listWidget_4.customContextMenuRequested.connect(lambda: self.rightMenuShow(self.listWidget_4))
         # 视频建模
-        self.pushButton_2.clicked.connect(self.Preview2)
-        self.pushButton_5.clicked.connect(self.add2)
-        self.listWidget_6.itemClicked.connect(self.QLWclicked2)
+        self.pushButton_2.clicked.connect(lambda: self.Preview(self.vacondition, self.varesult, self.lineEdit_2, self.textBrowser_5))
+        self.pushButton_5.clicked.connect(self.add)
         self.listWidget_6.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
         self.listWidget_6.customContextMenuRequested.connect(lambda: self.rightMenuShow(self.listWidget_6))
         # 生成式
-        self.listWidget_5.itemClicked.connect(self.QLWclicked3)
+        # self.listWidget_5.itemClicked.connect(self.QLWclicked3)
         self.listWidget_5.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
         self.listWidget_5.customContextMenuRequested.connect(lambda: self.rightMenuShow(self.listWidget_5))
 
         # 手工建模条件 - 1
         self.mmcondition = []
-        self.mmcountc = 1
+        self.mmcountc = 1       # 行数
         # 手工建模结果 - 2
         self.mmresult = []
         self.mmcountr = 1
@@ -56,169 +55,71 @@ class actForm(QtWidgets.QWidget, Ui_Frame):
         self.inif(self.gridLayout_19, self.mmresult, 2)
         self.inif(self.gridLayout_11, self.vacondition, 3)
         self.inif(self.gridLayout_14, self.varesult, 4)
-        # self.inif(self.gridLayout_19, self.mmresult, self.mmcountr, '结果')
-        # 增
 
-        # 删
+    # 预览按钮
+    def Preview(self, condition, result, name, textBrowser):
+        combobox = QComboBox()
+        lineEdit = QLineEdit()
+        con = [name.text()]
+        re = []
+        for i in condition:
+            temp = []
+            for j in i:
+                if type(j) == type(combobox):
+                    temp.append(j.currentText())
+                elif type(j) == type(lineEdit):
+                    temp.append(j.text())
+            con.append(temp)
+        for i in result:
+            temp = []
+            for j in i:
+                if type(j) == type(combobox):
+                    temp.append(j.currentText())
+                elif type(j) == type(lineEdit):
+                    temp.append(j.text())
+            re.append(temp)
+        self.tempModel = {name.text(): self.modeling(con, re)}
+        textBrowser.setText(self.tempModel[name.text()])
 
+    # 模型生成
+    def modeling(self, condition, result):
+        model = '(p '
+        model += condition[0] + '\n'
+        model += '\t=' + condition[1][0] + '>' + "\n"
+        model += "\t\t" + condition[1][1] + "\t\t" + condition[1][2] + "\n"
+        for con in condition[2:]:
+            if con[1] == 'goal' or con[1] == 'retrieval':
+                model += '\t' + con[0] + con[1] + '>\n'
+                model += '\t\t' + con[2] + '\t\t' + con[3]
+            else:
+                model += "\t\t" + con[1] + "\t\t" + con[2] + con[3] + "\n"
+        model += '==>\n'
+        model += '\t=' + result[0][0] + '>\n'
+        model += '\t\t'+ result[0][1] + '\t\t' + result[0][2] + '\n'
+        for re in result[1:]:
+            if re[1] == 'goal' or re[1] == 'retrieval':
+                model += '\t' + re[0] + re[1] + '>\n'
+                model += '\t\t' + re[2] + '\t\t' + re[3] + '\n'
+            else:
+                model += "\t\t" + re[1] + "\t\t" + re[2] + re[3] + "\n"
+        model += ')'
+        return model
 
-    # 获取手工建模的条件和结果
-    def get1(self):
-        condition = []
-        condition.append(self.lineEdit.text())
-        temp = []
-        temp.append(self.comboBox_3.currentText())
-        temp.append(self.comboBox_7.currentText())
-        temp.append(self.lineEdit_3.text())
-        condition.append(temp)
-        temp = []
-        temp.append(self.comboBox_9.currentText())
-        temp.append(self.comboBox_10.currentText())
-        temp.append(self.lineEdit_8.text())
-        condition.append(temp)
-
-        result = []
-        temp = []
-        temp.append(self.comboBox_4.currentText())
-        temp.append(self.comboBox_11.currentText())
-        temp.append(self.lineEdit_9.text())
-        result.append(temp)
-        temp = []
-        temp.append(self.comboBox_5.currentText())
-        temp.append(self.comboBox_12.currentText())
-        temp.append(self.lineEdit_4.text())
-        result.append(temp)
-        temp = []
-        temp.append(self.comboBox_6.currentText())
-        temp.append(self.comboBox_13.currentText())
-        temp.append(self.lineEdit_5.text())
-        result.append(temp)
-        return condition, result
-
-    def Preview1(self):
-        # print(self.get1())
-        condition, result = self.get1()
-        if condition[0] not in self.names:
-            self.textBrowser_4.setText(self.models.makeModel(condition, result))
-        else:
-            QMessageBox.information(self, "Information",
-                                    self.tr("已有该函数名"))
-
-    # 获取视频建模的条件和结果
-    def get2(self):
-        condition = []
-        condition.append(self.lineEdit_2.text())
-        try:
-            condition.append(float(self.lineEdit_6.text()))
-        except Exception as e:
-            QMessageBox.information(self, "Information",
-                                    self.tr("时间格式不正确"))
-        temp = []
-        temp.append(self.comboBox_21.currentText())
-        temp.append(self.comboBox_23.currentText())
-        temp.append(self.lineEdit_11.text())
-        condition.append(temp)
-        temp = []
-        temp.append(self.comboBox_20.currentText())
-        temp.append(self.comboBox_24.currentText())
-        temp.append(self.lineEdit_10.text())
-        condition.append(temp)
-
-        result = []
-        temp = []
-        temp.append(self.comboBox_17.currentText())
-        temp.append(self.comboBox_16.currentText())
-        temp.append(self.lineEdit_13.text())
-        result.append(temp)
-        temp = []
-        temp.append(self.comboBox_18.currentText())
-        temp.append(self.comboBox_15.currentText())
-        temp.append(self.lineEdit_7.text())
-        result.append(temp)
-        temp = []
-        temp.append(self.comboBox_19.currentText())
-        temp.append(self.comboBox_22.currentText())
-        temp.append(self.lineEdit_12.text())
-        result.append(temp)
-        return condition, result
-
-    def Preview2(self):
-        # print(self.get2())
-        condition, result = self.get2()
-        if condition[0] not in self.names:
-            # self.names.append(condition[0])
-            condition.pop(1)
-            self.MODELSTRING=self.models.makeModel(condition, result)
-            self.textBrowser_5.setText(self.MODELSTRING)
-            create_file2("d://test.lisp",self.MODELSTRING)
-
-        else:
-            QMessageBox.information(self, "Information",
-                                    self.tr("已有该函数名"))
-
-    def add1(self):
-        condition, result = self.get1()
-        if condition[0] not in self.models.name and condition[0] != '':
-            self.models.moremodel(condition, result)
-            self.listWidget_4.addItem(condition[0])
-            self.listWidget_5.addItem(condition[0])
-            self.listWidget_6.addItem(condition[0])
+    # 增加按钮
+    def add(self):
+        if list(self.tempModel.keys())[0] not in list(self.models.keys()):
+            self.models.update(self.tempModel)
+            self.listWidget_4.addItem(list(self.tempModel.keys())[0])
+            self.listWidget_5.addItem(list(self.tempModel.keys())[0])
+            self.listWidget_6.addItem(list(self.tempModel.keys())[0])
         else:
             QMessageBox.information(self, "Information",
                                     self.tr("添加未成功"))
 
-    def add2(self):
-        condition, result = self.get2()
-        if condition[0] not in self.models.name and condition[0] != '':
-            self.models.moremodel(condition, result)
-            self.listWidget_4.addItem(condition[0])
-            self.listWidget_5.addItem(condition[0])
-            self.listWidget_6.addItem(condition[0])
-        else:
-            QMessageBox.information(self, "Information",
-                                    self.tr("添加未成功"))
-
-    def delmodel(self, name):
-        num = self.models.name.index(name)
-        self.models.time.pop(num)
-        self.models.name.pop(num)
-        self.models.condition.pop(num)
-        self.models.result.pop(num)
-        self.models.model.pop(num)
-        self.models.len -= 1
-
-    # def add(self, verticalLayout1, verticalLayout2, verticalLayout3):
-    #     comboBox1 = QtWidgets.QComboBox(self.groupBox)
-    #     comboBox2 = QtWidgets.QComboBox(self.groupBox)
-    #     lineEdit = QtWidgets.QLineEdit(self.groupBox)
-    #     # 增加控件的名称
-    #     # self.comboBox_13.setObjectName("comboBox_13")
-    #     verticalLayout1.addWidget(comboBox1)
-    #     verticalLayout2.addWidget(comboBox2)
-    #     verticalLayout3.addWidget(lineEdit)
-    #
-    # def delete(self, verticalLayout1, verticalLayout2, verticalLayout3):
-    #     pass
-
-    def QLWclicked1(self, item):
-        # print(item.text())
-        try:
-            # print(self.models.name.index(item.text()))
-            self.textBrowser_4.setText(self.models.model[self.models.name.index(item.text())])
-        except Exception as e:
-            print(e)
-
-    def QLWclicked2(self, item):
-        try:
-            self.textBrowser_5.setText(self.models.model[self.models.name.index(item.text())])
-        except Exception as e:
-            print(e)
-
-    def QLWclicked3(self, item):
-        try:
-            self.textBrowser.setText(self.models.model[self.models.name.index(item.text())])
-        except Exception as e:
-            print(e)
+    # 知识列表点击功能
+    def QLWclicked(self, item):
+        print(item.text())
+        # textBrowser.setText(item.text())
 
     # 右键弹出菜单
     def rightMenuShow(self, qlw):
@@ -400,17 +301,6 @@ class actForm(QtWidgets.QWidget, Ui_Frame):
             comboboxs[2].addItems(symbol)
             comboboxs[3].addItems(list1)
             comboboxs[4].addItems(list2)
-
-    # def closeEvent(self, event):
-    #     title = self.names[0] + '.txt'
-    #     print(title)
-    #     try:
-    #         f = open(title, 'w')
-    #         for i in self.models.model:
-    #             f.write(i)
-    #         f.close()
-    #     except Exception as e:
-    #         print(e)
 
 
 if __name__ == '__main__':
